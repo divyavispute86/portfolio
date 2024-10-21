acafe5e3f5d366440bf5ef8ea85eb06235efb4e8
Caine Daniel <cainedaniel92@gmail.com>
2024-10-21 13:29:47 +0530

    Code Tweak

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  
-  basePath: '/portfolio/',
-  assetPrefix: '/portfolio/',
-}
+const nextConfig = {
+  output: 'export',  // Use 'export' to enable static export
+  basePath: '/divi-portfolio', // Set the base path if you are serving from a subdirectory
+  assetPrefix: '/divi-portfolio/', // Needed for loading assets correctly
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
